92e36473cc9f7c87470318e8dc4b53c4f9ab2f97
maybeyoung <stephen0607@qq.com>
2017-10-18 23:11:01 +0800

    添加了Hystrix的一些基本运用和方法

---
+++ microservice-comsumer-movie-ribbon-with-Hystrix/src/main/java/com/example/cloud/controller/MovieController.java
@@ -0,0 +1,41 @@
+package com.example.cloud.controller;
+
+import com.example.cloud.entity.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * Created by Administrator on 2017/10/11 0011.
+ */
+@RestController
+public class MovieController {
+
+     @Autowired
+    private RestTemplate restTemplate;
+
+     @GetMapping("/movie/{id}")
+     //在一个线程里执行
+     @HystrixCommand(fallbackMethod = "findByidFallback", commandProperties = {@HystrixProperty(name="execution.isolation.strategy", value="SEMAPHORE")})
+    public User findByisId(@PathVariable Long id){
+       return this.restTemplate.getForObject("http://microservice-provider-user/simple/"+id,User.class);
+    }
+
+    public User findByidFallback(Long id){
+        User user = new User();
+        user.setId(0L);
+        return user;
+    }
+
+
+
+
+}
