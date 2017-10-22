f3a7c7a6e2c15e11bb5e68cbcae133a42dea8de3
maybeyoung <stephen0607@qq.com>
2017-10-22 23:40:22 +0800

    Hystrix fallbackFactory方法 不能跟fallback放一起

--- microservice-comsumer-movie-feign-with-Hystrix/src/main/java/com/example/cloud/feign/UserFeignClient.java
+++ microservice-comsumer-movie-feign-with-Hystrix/src/main/java/com/example/cloud/feign/UserFeignClient.java
@@ -2,6 +2,9 @@ package com.example.cloud.feign;
 
 import com.example.cloud.entity.User;
 import com.example.config.Feign3Configuration;
+import feign.hystrix.FallbackFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
 
 import org.springframework.stereotype.Component;
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Created by Administrator on 2017/10/13 0013.
  */
-@FeignClient(name="microservice-provider-user",configuration = Feign3Configuration.class,fallback = HystrixClientFallback.class)
+@FeignClient(name="microservice-provider-user",/*configuration = Feign3Configuration.class,fallback = HystrixClientFallback.class,*/fallbackFactory =HystrixFallbackFactory.class )
 public interface UserFeignClient {
     //三个坑 1.@GetMapping不支持 2.@PathVariable得设置value
     // 3.只要参数是复杂对象，即使制定了GET方法，feign依然会以POST方法进行发送请求
@@ -29,4 +32,25 @@ class HystrixClientFallback implements UserFeignClient {
         user.setId(0L);
         return user;
     }
+}
+
+//fallbackFactory 如果需要访问导致回退触发的原因，可以使用fallbackFactory内部的属性@FeignClient
+@Component
+class HystrixFallbackFactory implements FallbackFactory<UserFeignClient> {
+
+  private static final Logger logger = LoggerFactory.getLogger(HystrixFallbackFactory.class);
+    @Override
+    public UserFeignClient create(Throwable throwable) {
+        HystrixFallbackFactory.logger.info("fallback; reason was: " + throwable.getMessage());
+          return id -> {
+                  User user = new User();
+                     user.setId(-1L);
+                  return user;
+
+          };
+    }
+}
+
+interface HystrixClientWithFallBackFactory extends UserFeignClient{
+
 }
